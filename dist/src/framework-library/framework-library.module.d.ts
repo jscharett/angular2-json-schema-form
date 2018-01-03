273918ad54ae97295726a1ac67991712640dfd6d
James Scharett <james.d.scharett@cbp.dhs.gov>
2018-01-03 11:17:00 -0500

    removed dist folder

--- dist/src/framework-library/framework-library.module.d.ts
+++
@@ -1,4 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export declare class FrameworkLibraryModule {
-    static forRoot(): ModuleWithProviders;
-}
--- dist/src/json-schema-form.module.d.ts
+++
@@ -1,4 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export declare class JsonSchemaFormModule {
-    static forRoot(): ModuleWithProviders;
-}
