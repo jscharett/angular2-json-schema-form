44aa2ec1cf21ef388910b9f37192083d11f4f261
James Scharett <james.d.scharett@cbp.dhs.gov>
2018-01-03 11:21:52 -0500

    0.5.1

---
+++ dist/angular2-json-schema-form.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './index';
+export { Bootstrap3FrameworkModule as ɵc } from './src/framework-library/bootstrap-3-framework/bootstrap-3-framework.module';
+export { MATERIAL_DESIGN_COMPONENTS as ɵb } from './src/framework-library/material-design-framework/index';
+export { BASIC_WIDGETS as ɵa } from './src/widget-library/index';
---
+++ dist/src/framework-library/material-design-framework/flex-layout-section.component.d.ts
@@ -0,0 +1,21 @@
+import { OnInit } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { JsonSchemaFormService } from '../../json-schema-form.service';
+export declare class FlexLayoutSectionComponent implements OnInit {
+    private jsf;
+    formControl: AbstractControl;
+    controlName: string;
+    controlValue: any;
+    controlDisabled: boolean;
+    boundControl: boolean;
+    options: any;
+    expanded: boolean;
+    containerType: string;
+    layoutNode: any;
+    layoutIndex: number[];
+    dataIndex: number[];
+    constructor(jsf: JsonSchemaFormService);
+    ngOnInit(): void;
+    toggleExpand(): void;
+    getFlexAttribute(attribute: string): any;
+}
